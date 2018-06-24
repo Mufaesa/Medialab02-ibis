abfe74f60b9f208699b4368da775bfc151e1d2a2
Tom Vrijmoet <tfjvrijmoet@hotmail.com>
2018-06-24 16:06:46 +0200

    Final changes

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ table.addEventListener("click", function() {
     var sidebar = document.getElementById("sidebar");
     sidebar.style.display = "inline-block";
 });
+
+var reserveBtn = document.getElementById("btn");
+reserveBtn.addEventListener("click", function(){
+  table.style.backgroundImage = "url('images/tafeltje-gereserveerd.PNG')";
+  sidebar.style.display = "none";
+  map.classList.remove("clicked");
+});
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+var timepicker = new TimePicker('time', {
+    lang: 'en',
+    theme: 'dark'
+  });
+  timepicker.on('change', function(evt) {
+    
+    var value = (evt.hour || '00') + ':' + (evt.minute || '00');
+    evt.element.value = value;
+  
+  });
